d5293583ba50f3d0bb74cac70f2b2f2d42085fee
davidparra <david.parra@utp.edu.co>
2016-10-12 10:09:11 -0500

    adding env to DB connection

--- config/models.js
+++ config/models.js
@@ -17,7 +17,7 @@ module.exports.models = {
   * connections (see `config/connections.js`)                                *
   *                                                                          *
   ***************************************************************************/
-    connection: 'defaultConnection',  //sqlserver
+    connection: process.env.CONNDB,  //sqlserver
 
   /***************************************************************************
   *                                                                          *
